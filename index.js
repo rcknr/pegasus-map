abcf535d9ad7beec4295c848f560028b48f904da
Sergii Kauk <sergii@kauk.at>
2021-07-05 19:25:50 +0200

    Set up hosting

--- index.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-const fs = require('fs');
-
-fetch('https://tle.ivanstanojevic.me/api/tle/42784')
-  .then(response => response.json())
-  .then(({ name, line1, line2 }) => {
-    const tle = [name, line1, line2].join('\\n');
-    fs.writeFile('dist/pegasus.js', `window._tle = "${tle}";\n`, e => console.error(e));
-  });
